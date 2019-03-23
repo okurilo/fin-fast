a9d108984e574915e403384655300361b8a86a89
okurilo <o.kurilo@molga.ru>
2019-03-23 20:38:32 +0300

    Updates

---
+++ precache-manifest.c7c2146a25bcd28cab24e23157b4b317.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e171e0b80ce45b7b606f",
+    "url": "/static/js/main.3cd033ff.chunk.js"
+  },
+  {
+    "revision": "ce2b593fb4039f31810a",
+    "url": "/static/js/2.1342971c.chunk.js"
+  },
+  {
+    "revision": "e171e0b80ce45b7b606f",
+    "url": "/static/css/main.e764e8ef.chunk.css"
+  },
+  {
+    "revision": "ce2b593fb4039f31810a",
+    "url": "/static/css/2.b2c544d2.chunk.css"
+  },
+  {
+    "revision": "ff096501fbbdb26ec8c0ef60b958daab",
+    "url": "/index.html"
+  }
+];
