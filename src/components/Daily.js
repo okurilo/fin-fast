a2c3d59863c976c05f8b22a722195c86c4da5021
okurilo <o.kurilo@molga.ru>
2019-05-07 16:58:25 +0300

    [ADD] Switch off detail vis by default

--- src/components/Daily.js
+++ src/components/Daily.js
@@ -11,7 +11,7 @@ export default class Daily extends Component {
       super(props);
 
       this.state = {
-         showDetail: true
+         showDetail: false
       };
     }
     handleSwitchDetailShow = () => {
